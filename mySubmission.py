ba03f0cebfde4f60b8b5ed3540dae6b3072566e2
ldueker <ldueker2@gmail.com>
2024-01-31 15:35:01 -0700

    changes, still WIP

--- mySubmission.py
+++ mySubmission.py
@@ -1,49 +1,59 @@
 import sys
 import math
 
-def euclidean_distance(point1, point2):
-    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
 
 def total_drive_time(route, loads):
-    total_time = 0
+    totalTime = 0
     for i in range(len(route) - 1):
-        pickup_loc = loads[route[i]][1]
-        dropoff_loc = loads[route[i]][2]
-        next_pickup_loc = loads[route[i + 1]][1]
-        total_time += euclidean_distance((0, 0), pickup_loc) + euclidean_distance(pickup_loc, dropoff_loc) + euclidean_distance(dropoff_loc, next_pickup_loc)
-    return total_time
+        pickupLocation = loads[route[i]][1]
+        dropoffLocation = loads[route[i]][2]
+        nextPickupLocation = loads[route[i + 1]][1]
+        totalTime += euclideanDistance((0, 0), pickupLocation) + euclideanDistance(pickupLocation, dropoffLocation) + euclideanDistance(dropoffLocation, nextPickupLocation)
+    return totalTime
 
-def clarke_wright_savings(loads):
-    n = len(loads)
+
+
+def euclideanDistance(p1, p2):
+    return math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+def optimizeRoutes(loads):
+    numLoads = len(loads)
     savings = []
-
-    for i in range(n):
-        for j in range(i + 1, n):
+    for i in range(numLoads):
+        for j in range(i + 1, numLoads):
             saving = total_drive_time([i, j], loads) - total_drive_time([i], loads) - total_drive_time([j], loads)
             savings.append((i, j, saving))
-
+    
+    #Sorting my routes
     savings.sort(key=lambda x: x[2], reverse=True)
-
-    routes = [[i] for i in range(n)]
+    
+    #constructing route number
+    routes = [[i] for i in range(numLoads)]
     
     #Comparing routes to see if routes can be combined
     for i, j, saving in savings:
-        route_i = next((r for r in routes if i in r), None)
-        route_j = next((r for r in routes if j in r), None)
+        route1 = next((r for r in routes if i in r), None)
+        route2 = next((r for r in routes if j in r), None)
 
         #Combine Routes if Possible
-        if route_i and route_j and route_i != route_j:
-            merged_route = route_i + route_j[::-1]
-            if total_drive_time(merged_route, loads) <= 12 * 60:
-                routes.remove(route_i)
-                routes.remove(route_j)
-                routes.append(merged_route)
+        if route1 and route2 and route1 != route2:
+            mergedRoute = route1 + route2[::-1]
+            if total_drive_time(mergedRoute, loads) <= 12 * 60:
+                routes.remove(route1)
+                routes.remove(route2)
+                routes.append(mergedRoute)
+
+
+    for i in range(len(routes)):
+        current_route = routes[i]
+        for j in range(1, len(current_route) - 1):
+            test_route = current_route[:j] + current_route[j+1:]
+            test_route.insert(j, current_route[j])
+            if total_drive_time(test_route, loads) <= 12 * 60:
+                routes[i] = test_route
 
     return routes
 
-def print_solution(routes):
-    for route in routes:
-        print(route)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -56,11 +66,14 @@ if __name__ == "__main__":
         file.readline()
         loads = []
         for line in file:
-            parts = line.split()
-            load_number = int(parts[0])
-            pickup_loc = tuple(map(float, parts[1][1:-1].split(',')))
-            dropoff_loc = tuple(map(float, parts[2][1:-1].split(',')))
-            loads.append((load_number, pickup_loc, dropoff_loc))
+            column = line.split()
+            loadNum = int(column[0])
+            pickupLocation = tuple(map(float, column[1][1:-1].split(',')))
+            dropoffLocation = tuple(map(float, column[2][1:-1].split(',')))
+            loads.append((loadNum, pickupLocation, dropoffLocation))
 
-    routes = clarke_wright_savings(loads)
-    print_solution(routes)
+    routes = optimizeRoutes(loads)
+    routes = [[n+1 for n in sub] for sub in routes]
+    for route in routes:
+        print(route)
+
